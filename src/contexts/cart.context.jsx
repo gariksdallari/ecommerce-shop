dc641823313108df8eaf6af3981996403e466bf6
gariks.dallari <gariks.dallari@dynamicyield.com>
2023-03-16 10:48:16 +0200

    cart context updated with reducers

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState, useEffect, useReducer } from "react";
 
 const addCartItem = (cartItems, productToAdd) => {
   const existingCartItem = cartItems.find(
@@ -23,11 +23,61 @@ export const CartContext = createContext({
   cartCount: 0,
 });
 
+export const CART_ACTION_TYPES = {
+  SET_IS_CART_OPEN: "SET_IS_CART_OPEN",
+  SET_CART_ITEMS: "SET_CART_ITEMS",
+  SET_CART_COUNT: "SET_CART_COUNT",
+};
+
+const cartReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case CART_ACTION_TYPES.SET_IS_CART_OPEN:
+      return {
+        ...state,
+        isCartOpen: payload,
+      };
+    case CART_ACTION_TYPES.SET_CART_ITEMS:
+      return {
+        ...state,
+        cartItems: payload,
+      };
+    case CART_ACTION_TYPES.SET_CART_COUNT:
+      return {
+        ...state,
+        cartCount: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  isCartOpen: false,
+  cartItems: [],
+  cartCount: 0,
+};
+
 export const CartProvider = ({ children }) => {
-  const [isCartOpen, setIsCartOpen] = useState(false);
-  const [cartItems, setCartItems] = useState([]);
-  const [cartCount, setCartCount] = useState(0);
   const [totalPrice, setTotalPrice] = useState(0);
+
+  const [state, dispatch] = useReducer(cartReducer, INITIAL_STATE);
+
+  const { isCartOpen, cartItems, cartCount } = state;
+
+  const setCartItems = (items) => {
+    dispatch({type: CART_ACTION_TYPES.SET_CART_ITEMS, payload: items})
+  };
+
+  const setCartCount = (count) => {
+    dispatch({type: CART_ACTION_TYPES.SET_CART_COUNT, payload: count})
+  };
+
+  const setIsCartOpen = (bool) => {
+    dispatch({type: CART_ACTION_TYPES.SET_IS_CART_OPEN, payload: bool})
+  }
+
 
   const incrementItemCount = (item) => {
     const target = cartItems.find((el) => el.id === item.id);
@@ -36,24 +86,30 @@ export const CartProvider = ({ children }) => {
 
   const decrementItemCount = (item) => {
     const target = cartItems.find((el) => el.id === item.id);
-    if(target.quantity === 1) return;
+    if (target.quantity === 1) return;
     setCartItems([...cartItems], target.quantity--);
   };
 
   const removeItem = (item) => {
-    const res = cartItems.filter(el => el.id !== item.id);
+    const res = cartItems.filter((el) => el.id !== item.id);
     setCartItems([...res]);
-  }
+  };
 
   useEffect(() => {
     const count = cartItems.reduce((acc, item) => acc + item.quantity, 0);
-    const totalPrice = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    const totalPrice = cartItems.reduce(
+      (acc, item) => acc + item.price * item.quantity,
+      0
+    );
     setTotalPrice(totalPrice);
     setCartCount(count);
   }, [cartItems]);
 
   useEffect(() => {
-    const totalPrice = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0);
+    const totalPrice = cartItems.reduce(
+      (acc, item) => acc + item.price * item.quantity,
+      0
+    );
     setTotalPrice(totalPrice);
   }, [cartItems]);
 
@@ -71,7 +127,7 @@ export const CartProvider = ({ children }) => {
     incrementItemCount,
     decrementItemCount,
     removeItem,
-    totalPrice
+    totalPrice,
   };
 
   return <CartContext.Provider value={value}>{children}</CartContext.Provider>;
