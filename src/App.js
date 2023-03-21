06a1478827794449c194f5007f933888a190c272
gariks.dallari <gariks.dallari@dynamicyield.com>
2023-03-21 11:49:07 +0200

    setting redux toolkit

--- src/App.js
+++ src/App.js
@@ -12,11 +12,10 @@ import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-import { setCurrentUser } from './store/user/user.action';
+import { setCurrentUser } from './store/user/user.reducer';
 
 const App = () => {
   const dispatch = useDispatch();
-console.log('aaaaa');
   useEffect(() => {
     const unsubscribe = onAuthStateChangedListener((user) => {
       if (user) {
